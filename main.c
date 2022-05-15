45cf8e6fa68437b553059efc96d1c5511deaff11
Your Name <you@example.com>
2022-05-15 18:30:01 -0400

    Listas las struct y el menú

--- main.c
+++ main.c
@@ -7,7 +7,105 @@
 #include "list.h"
 #include "treemap.h"
 
+typedef struct
+{
+    int id;
+    char titulo[100];
+    long cantPalabras;
+    long cantCaracteres;
+    Map* mapaPalabras;
+} tipoLibro;
+
+typedef struct
+{
+    int apariciones;
+    char palabra[100];
+    List* lineaPalabra;
+    List* libroAsociado;
+} tipoPalabra;
+
+/*
+  función para comparar claves de tipo string
+  retorna 1 si son iguales
+*/
+int is_equal_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2)==0) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo string
+  retorna 1 si son key1<key2
+*/
+int lower_than_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2) < 0) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo int
+  retorna 1 si son iguales
+*/
+int is_equal_int(void * key1, void * key2) {
+    if(*(int*)key1 == *(int*)key2) return 1;
+    return 0;
+}
+
+/*
+  función para comparar claves de tipo int
+  retorna 1 si son key1<key2
+*/
+int lower_than_int(void * key1, void * key2) {
+    if(*(int*)key1 < *(int*)key2) return 1;
+    return 0;
+}
+
+void cargarDocumentos (char* nombreLibros)
+{
+
+}
+
 int main()
 {
+    char* nombreLibros = (char*) malloc (sizeof(char));
+    int option;
+
+    //Creación de menú
+    while (option != 0)
+    {
+        printf("******************************************\n");
+        printf("1. Cargar documentos\n");
+        printf("2. Mostrar documentos ordenados\n");
+        printf("3. Buscar un libro por titulo\n");
+        printf("4. Mostrar palabras con mayor frecuencia en un documento\n");
+        printf("5. Mostrar palabras mas relevantes en un documento\n");
+        printf("6. Buscar por palabra\n");
+        printf("7. Mostrar palabras en el contexto del documento\n");
+        printf("0. Salir\n");
+        printf("******************************************\n");
+
+        printf("Indique su opcion: ");
+        scanf("%d", &option);
+
+        switch(option)
+        {
+            case 1: printf("FUNCION NO IMPLEMENTADA!\n");
+                    break;
+            case 2: printf("FUNCION NO IMPLEMENTADA!\n");
+                    break;
+            case 3: printf("FUNCION NO IMPLEMENTADA!\n");
+                    break;
+            case 4: printf("FUNCION NO IMPLEMENTADA!\n");
+                    break;
+            case 5: printf("FUNCION NO IMPLEMENTADA!\n");
+                    break;
+            case 6: printf("FUNCION NO IMPLEMENTADA!\n");
+                    break;
+            case 7: printf("FUNCION NO IMPLEMENTADA!\n");
+                    break;
+            case 0: break;
+        }
+        printf("\n");
+    }
     return 0;
 }
